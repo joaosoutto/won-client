73f4f0215ac98298de2bace3933106300e79d8d7
joaosoutto <joaosoutto92@gmail.com>
2021-09-07 17:44:09 -0300

    #logo, #mediaquery, #testsOnLogo

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,43 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render black label with props', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render a default logo', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width: 768px)' }
+    )
+  })
+})
---
+++ src/utils/tests/helpers.tsx
@@ -0,0 +1,8 @@
+// Helper to use theme on tests files
+import { ThemeProvider } from 'styled-components'
+import { render, RenderResult } from '@testing-library/react'
+
+import theme from 'styles/theme'
+
+export const renderWithTheme = (children: React.ReactNode): RenderResult =>
+  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
