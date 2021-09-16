32b0b30b759c8da40d4f69edecd43dcc3a1b5ae3
joaosoutto <joaosoutto@hotmail.com>
2021-09-16 18:58:08 -0300

    Boilerplate breakpoint / example of props on pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
 import Home from 'templates/Home'
 
+// export default function Index(props: type here) {
+//   return <Home {...props} />
+// }
+
 export default function Index() {
   return <Home />
 }
+
+// ATENTION:
+// Methods like getStaticProps or getServerSideProps ONLY WOKR ON /pages !!
+
+// getStaticProps: generate static in build time
+// getServerSideProps: gerenate via SSR on each request
+
+// export function getServerSideProps/getStaticProps() {
+//   make some logic here
+//   consume an API
+//   context reading
+
+//    then return data:
+//    return {
+//      props: {
+//        data here
+//      }
+//    }
+//  }
