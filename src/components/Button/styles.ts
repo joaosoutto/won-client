e67737b946bac388e59aede21359f49f3258b94e
joaosoutto <joaosoutto@hotmail.com>
2021-10-15 13:05:46 -0300

    PaymentOptions

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,12 +1,13 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+import { darken } from 'polished'
+
 import { ButtonProps } from '.'
 
-export type WrapperProps = { hasIcon: boolean } & Pick<
-  ButtonProps,
-  'size' | 'fullWidth' | 'minimal'
->
+export type WrapperProps = {
+  hasIcon: boolean
+} & Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'>
 
- const WrapperModifiers = {
+const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
     height: 3rem;
     font-size: ${theme.font.sizes.xsmall};
@@ -27,39 +28,48 @@ export type WrapperProps = { hasIcon: boolean } & Pick<
   withIcon: (theme: DefaultTheme) => css`
     svg {
       width: 1.5rem;
-
       & + span {
-        margin-left: ${theme.spacings.xsmall};
+        margin-left: ${theme.spacings.xxsmall};
       }
     }
   `,
   minimal: (theme: DefaultTheme) => css`
-  background: none;
-  color: ${theme.colors.primary};
+    background: none;
+    color: ${theme.colors.primary};
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+    }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
-    border: none;
+    font-family: ${theme.font.family};
+    border: 0;
     cursor: pointer;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
-    transition: all 0.3s;
     text-decoration: none;
-
     &:hover {
-      background: ${minimal ? 'none' : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
+      background: ${minimal
+        ? 'none'
+        : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
-
-    ${!!size && WrapperModifiers[size](theme)}
-    ${!!fullWidth && WrapperModifiers.fullWidth()}
-    ${!!hasIcon && WrapperModifiers.withIcon(theme)}
-    ${!!minimal && WrapperModifiers.minimal(theme)}
+    ${!!size && wrapperModifiers[size](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)};
+    ${!!minimal && wrapperModifiers.minimal(theme)};
+    ${disabled && wrapperModifiers.disabled()};
   `}
 `
